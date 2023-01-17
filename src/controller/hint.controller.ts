69628bcf7f6f64715904a4319f0ff76275a26b9d
Giang Bui <bhuonggiang03@gmail.com>
2023-01-17 01:32:40 -0500

    implement feedback api, and more hint results

--- src/controller/hint.controller.ts
+++ src/controller/hint.controller.ts
@@ -24,19 +24,31 @@ export class HintController {
     };
   }
 
-  @Post('hint/:language/:id')
-  async getHint(@Param() params): Promise<unknown> {
+  @Post('hints/:language/:id')
+  async getHint(@Param() params, @Body() body): Promise<unknown> {
+    console.log(body);
     return {
-      type: 'code',
-      hint: 'x = 4\ny = 5',
-      // 'hint': 'How does your code handle if the number of followers is less than 0?\n',
+      id: 1,
+      type: 'text',
+      // hint: 'x = 4\ny = 5',
+      hint: '\n1) Should we check if the number of followers is greater than or equal to 0 before checking if the number of followers is less than 10?\n\n- Yes, because if the number of followers is less than 0, then it does not make sense to compare it to 10.\n\n2) Should we check if the number of followers is greater than or equal to 10 before checking if the number of followers is less than 100?\n\n- Yes, because if the number of followers is less than 10, then it does not make sense to compare it to 100.\n\n3) Should we check if the number of followers is greater than or equal to 100 before printing ""expert""?\n\n- No, because if the number of followers is greater than or equal to 100, we can print ""expert"".',
       config: {
-        title: 'Hint',
+        // title: 'Hint',
         description: 'This is your hint:',
         level: 2, // 1 means all content is shown at once, 2 means the detailed hints are hidden(on toggle)
         more: true,
         feedback: true,
       },
+    };
+  }
+
+  @Post('feedback/:language/:id')
+  async sendFeedback(@Param() params, @Body() body): Promise<unknown> {
+    console.log(body);
+    // throw new Error();
+    return {
+      message: 'Feedback received',
+      showTextFeedback: true,
     };
   }
 }
