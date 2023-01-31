2ea455e903a6fcaa314d41d2b4c6d289bcf31eb2
nicholaas11 <nicholas.susanto02@gmail.com>
2023-01-31 18:05:49 -0500

    Add get hint function

--- src/controller/hint.controller.ts
+++ src/controller/hint.controller.ts
@@ -44,20 +44,24 @@ export class HintController {
 
   @Post('hints/:language/:pk') // problem id
   async getHint(@Param() params, @Body() body): Promise<unknown> {
-    console.log(body);
-    return {
-      id: 1,
-      type: 'text',
-      // hint: 'x = 4\ny = 5',
-      hint: '\n1) Should we check if the number of followers is greater than or equal to 0 before checking if the number of followers is less than 10?\n\n- Yes, because if the number of followers is less than 0, then it does not make sense to compare it to 10.\n\n2) Should we check if the number of followers is greater than or equal to 10 before checking if the number of followers is less than 100?\n\n- Yes, because if the number of followers is less than 10, then it does not make sense to compare it to 100.\n\n3) Should we check if the number of followers is greater than or equal to 100 before printing ""expert""?\n\n- No, because if the number of followers is greater than or equal to 100, we can print ""expert"".',
-      config: {
-        // title: 'Hint',
-        // description: 'This is your hint:',
-        level: 2, // 1 means all content is shown at once, 2 means the detailed hints are hidden(on toggle)
-        more: true,
-        feedback: true,
-      },
-    };
+    const prompt = "What are the steps to improve the following sql code?\n" + body.submission.slice(40,-41);
+    const student = await this.studentService.findByUid(body.uid);
+    const problem = await this.problemService.findByPk(params.pk, params.language);
+
+    return await this.hintService.create(prompt, student, problem);
+    // return {
+    //   id: 1,
+    //   type: 'text',
+    //   // hint: 'x = 4\ny = 5',
+    //   hint: '\n1) Should we check if the number of followers is greater than or equal to 0 before checking if the number of followers is less than 10?\n\n- Yes, because if the number of followers is less than 0, then it does not make sense to compare it to 10.\n\n2) Should we check if the number of followers is greater than or equal to 10 before checking if the number of followers is less than 100?\n\n- Yes, because if the number of followers is less than 10, then it does not make sense to compare it to 100.\n\n3) Should we check if the number of followers is greater than or equal to 100 before printing ""expert""?\n\n- No, because if the number of followers is greater than or equal to 100, we can print ""expert"".',
+    //   config: {
+    //     // title: 'Hint',
+    //     // description: 'This is your hint:',
+    //     level: 2, // 1 means all content is shown at once, 2 means the detailed hints are hidden(on toggle)
+    //     more: true,
+    //     feedback: true,
+    //   },
+    // };
   }
 
   @Post('feedback/:language/:id')
--- src/service/hint.service.ts
+++ src/service/hint.service.ts
@@ -2,6 +2,12 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Hint } from 'src/entity/hint.entity';
 import { FindOneOptions, Repository } from 'typeorm';
+import { Configuration, OpenAIApi } from 'openai';
+import { HintType, LanguageType } from 'src/types';
+import { StudentService } from './student.service';
+import { Student } from 'src/entity/student.entity';
+import { Problem } from 'src/entity/problem.entity';
+
 
 @Injectable()
 export class HintService {
@@ -17,8 +23,34 @@ export class HintService {
     });
   }
 
-  async create(): Promise<Hint> {
-    return;
+  async create(
+    prompt: string,
+    student: Student,
+    problem: Problem
+    ): Promise<Hint> {
+    const configuration = new Configuration({
+      apiKey: process.env.OPENAI_API_KEY,
+    });
+    const openai = new OpenAIApi(configuration);
+    
+    const response = await openai.createCompletion({
+      model: "code-davinci-002",
+      prompt: prompt,
+      temperature: 0,
+      max_tokens: 256,
+      top_p: 1,
+      frequency_penalty: 0,
+      presence_penalty: 0,
+    });
+
+
+    const hint = await this.hintRepository.create({
+      hint: response.data.choices[0].text,
+      student: student,
+      type: HintType.TEXT,
+      problem: problem
+    })
+    return hint;
   }
 
   async saveFeeback(
