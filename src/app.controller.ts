523e564cad51e6610d0c5cb067415fff16219d2e
Giang Bui <bhuonggiang03@gmail.com>
2023-02-01 14:37:46 -0500

    update auth endpoint

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -10,7 +10,7 @@ export class AppController {
     return this.appService.getHello();
   }
 
-  @Get('/domainAuth')
+  @Get('/.well-known/pki-validation/7F1464B6B2E19AA18E9DD5DF41BDDC4C.txt')
   getDomainAuth(): string {
     return this.appService.getDomainAuth();
   }
