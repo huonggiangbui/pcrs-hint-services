13d08bf70e3bd4649988c1c10bd18e8e683ae525
Giang Bui <bhuonggiang03@gmail.com>
2023-02-08 15:19:39 -0500

    update configuration

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,5 @@
 import { HintType } from './types';
 
-export const HINT_BUTTON_TEXT = ['Get hint'];
 export const PROMPT_HEADERS = {
   "Steps to fix student's code:\n1.": HintType.TEXT,
   "Suggestions to improve student's code:\n\n": HintType.CODE,
@@ -9,5 +8,6 @@ export const MORE_HINT_PROMPT_HEADERS = {
   'Explain the steps above:\n-': HintType.TEXT,
   "More edit suggestions to improve student's code:\n\n": HintType.CODE,
 };
-export const HINT_TITLE = ['Hint'];
+export const HINT_BUTTON_TEXT = ['Get a hint', 'Get a next step', 'Help'];
+export const HINT_TITLE = ['Your hint', 'Next step …'];
 export const HINT_DESCRIPTION = ['This is your hint'];
