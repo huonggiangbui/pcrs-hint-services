f499d698e91395671ea791b58a5729babc108005
Giang Bui <bhuonggiang03@gmail.com>
2023-02-09 23:06:04 -0500

    change orange color var

--- src/constants.ts
+++ src/constants.ts
@@ -9,6 +9,6 @@ export const MORE_HINT_PROMPT_HEADERS = {
   "More edit suggestions to improve student's code:\n\n": HintType.CODE,
 };
 export const HINT_BUTTON_TEXT = ['Get a hint', 'Get a next step', 'Help'];
-export const HINT_BUTTON_COLOR = ['#002A5C', '#017100', '#FA5C00'];
+export const HINT_BUTTON_COLOR = ['#002A5C', '#017100', '#d2430f'];
 export const HINT_TITLE = ['Your hint', 'Next step …'];
 export const HINT_DESCRIPTION = ['This is your hint'];
