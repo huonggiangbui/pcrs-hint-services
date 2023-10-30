edce83fd1075c6a18626516b13c8bf539a0d1791
Giang Bui <bhuonggiang03@gmail.com>
2023-10-29 22:58:46 -0400

    add error handling for problem api

--- src/service/problem.service.ts
+++ src/service/problem.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { CreateProblemDto } from 'src/dto/create-problem';
 import { UpdateProblemDto } from 'src/dto/update-problem';
@@ -18,7 +18,7 @@ export class ProblemService {
     language: LanguageType,
     options?: FindOneOptions<Problem>,
   ): Promise<Problem> {
-    const problem = await this.problemRepository.findOneOrFail({
+    const problem = await this.problemRepository.findOne({
       where: { pk, language },
       ...options,
     });
@@ -30,6 +30,11 @@ export class ProblemService {
     language: LanguageType,
     data: CreateProblemDto,
   ): Promise<Problem> {
+    if (this.findByPk(data.pk, language)) {
+      Logger.error(`Problem not found: ${data.pk}, language: ${language}`);
+      return null;
+    }
+
     const problem = this.problemRepository.create({
       ...data,
       language: language,
