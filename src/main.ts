f919a627fe06b665aec3aae09b2019c8492384b3
Giang Bui <bhuonggiang03@gmail.com>
2023-11-01 02:22:40 -0400

    remove api key guard

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ApiKeyGuard } from './guards/key-auth.guard';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
@@ -9,7 +8,6 @@ async function bootstrap() {
   });
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
-  app.useGlobalGuards(new ApiKeyGuard());
   const port = process.env.PORT || 3333;
   app.enableCors({
     origin: process.env.ALLOWED_ORIGINS.split(',') || '',
